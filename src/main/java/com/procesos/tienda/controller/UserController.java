d9b36f850baa137289972b23b415b64b03e43752
wilder cepeda <wilder.cepeda2002@gmail.com>
2023-09-19 20:37:37 -0500

    aquí va el mensaje alusivo al cambio realizado

--- src/main/java/com/procesos/tienda/controller/UserController.java
+++ src/main/java/com/procesos/tienda/controller/UserController.java
@@ -5,17 +5,34 @@ import com.procesos.tienda.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
     @GetMapping("users/{id}")
-    public User getUserById(@PathVariable String id){
-        User user = new User();
-        return user;
+    public User getUserById(@PathVariable long id){
+       return  userService.getUserById(id);
     }
+
     @PostMapping("users")
     public User create(@RequestBody User user){
         return userService.createUser(user);
+    }
+
+    @PutMapping ("users/{id}")
+    public User update(@RequestBody User user,@PathVariable long id){
+
+        return userService.updateUser(user,id);
+    }
+    @DeleteMapping ("users/{id}")
+    public Boolean delete(@PathVariable Long id){
+
+        return userService.deleteUser(id);
+    }
+    @GetMapping("users")
+    public List<User> findtAll(){
+        return userService.findAllUsers();
     }
 }
--- src/main/java/com/procesos/tienda/service/UserService.java
+++ src/main/java/com/procesos/tienda/service/UserService.java
@@ -5,6 +5,7 @@ import com.procesos.tienda.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,11 +16,11 @@ public class UserService {
     public User createUser(User userReq){
         return userRepository.save(userReq);
     }
-    public Optional<User> getUserById(Long id){
-        return userRepository.findById(id);
+    public User getUserById(Long id){
+        return userRepository.findById(id).get();
     }
-    public User updateUser(User userReq){
-        Optional<User> userBd = userRepository.findById(userReq.getId());
+    public User updateUser(User userReq, Long id){
+        Optional<User> userBd = userRepository.findById(id);
         if(userBd.isEmpty()){
             return null;
         }
@@ -37,5 +38,8 @@ public class UserService {
         }
         userRepository.delete(userBd.get());
         return true;
+    }
+    public List<User> findAllUsers(){
+        return  (List<User>) userRepository.findAll();
     }
 }
